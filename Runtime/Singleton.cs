0cd5cab9a2187673fc31f5038fb114c5ace82a16
Werefox <jack@wrightus.com>
2023-07-21 14:50:37 -0400

    Copied Singleton.cs from a previous project and created an assembly file

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxthorne
+{
+	public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+	{
+		public static T Instance { get; private set; }
+
+		[Header("Singleton")]
+		public bool Persistent = true;
+
+		protected virtual void Awake()
+		{
+			if (Instance != null && Instance != this)
+			{
+				Debug.Log("Duplicate singleton " + typeof(T).Name + " found, destroying");
+				Destroy(gameObject);
+				return;
+			}
+			else
+			{
+				DontDestroyOnLoad(this);
+				Instance = this as T;
+			}
+		}
+	}
+}
