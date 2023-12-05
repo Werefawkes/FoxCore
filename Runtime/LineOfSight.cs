510f5e88bb74f185b59eb9c0e5b8bb8124f15471
Werefox <jack@wrightus.com>
2023-12-05 18:52:22 -0500

    Implemented visibility method

--- Runtime/LineOfSight.cs
+++ Runtime/LineOfSight.cs
@@ -8,17 +8,29 @@ namespace Foxthorne.Utilities
     public class LineOfSight : MonoBehaviour
     {
         public List<Vector3> points = new();
+		public LayerMask LayerMask;
 
-        public float VisibilityFrom(Vector3 position)
+        public float GetVisibilityFrom(Vector3 position)
 		{
-			throw new NotImplementedException();
+			int hitCount = 0;
+			foreach (Vector3 p in points)
+			{
+				Vector3 target = transform.position + p;
+				if (!Physics.Raycast(position, target - position, Vector3.Distance(position, target), LayerMask))
+				{
+					hitCount++;
+				}
+			}
+
+			Debug.Log(hitCount);
+			return (float)hitCount / points.Count;
 		}
 
 		private void OnDrawGizmosSelected()
 		{
 			foreach (Vector3 p in points)
 			{
-				Gizmos.DrawSphere(transform.position + p, 0.1f);
+				Gizmos.DrawSphere(transform.position + p, 0.05f);
 			}
 		}
 	}
